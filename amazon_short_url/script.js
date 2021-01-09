09a834e451e5b85dfa8478a14a96fd568fcb965b
kabik <k2.s1k.x20@gmail.com>
2021-01-09 21:01:27 +0900

    remove console log

--- amazon_short_url/script.js
+++ amazon_short_url/script.js
@@ -9,15 +9,12 @@ function copy() {
     let notifyTarget = document.getElementById("notify")
     const copyTarget = document.querySelector('#short_url')
 
-    console.log(copyTarget)
     document.getSelection().selectAllChildren(copyTarget)
     document.execCommand("copy")
     document.getSelection().empty(copyTarget)
 
-    console.log("copied")
     notifyTarget.classList.add('is-show')
     setTimeout(() => {
         notifyTarget.classList.remove('is-show')
-        console.log("remove")
     }, 1000)
 }
