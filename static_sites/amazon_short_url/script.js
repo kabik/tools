a2311957ab0da98e433e88321c9e7d6ac43fcb59
kabik <k2.s1k.x20@gmail.com>
2021-01-10 15:33:50 +0900

    Add link

--- static_sites/amazon_short_url/script.js
+++ static_sites/amazon_short_url/script.js
@@ -1,14 +1,22 @@
 const TOP_URL = "https://www.amazon.co.jp"
 
-function shortenUrl() {
+function getShortUrl() {
     let origUrl = document.getElementById("orig_url").value
     let dp = origUrl.match("/dp/[a-zA-Z0-9]*/")
     let gp = origUrl.match("/gp/product/[a-zA-Z0-9]*/")
+
     if (dp) {
-        document.getElementById("short_url").innerText = TOP_URL + dp[0]
+        return TOP_URL + dp[0]
     } else if (gp) {
-        document.getElementById("short_url").innerText = TOP_URL + gp[0]
+        return TOP_URL + gp[0]
     }
+}
+
+function shortenUrl() {
+    let shortUrl = getShortUrl()
+    let target = document.getElementById("short_url")
+    target.href = shortUrl
+    target.innerHTML = shortUrl
 }
 
 function copy() {
