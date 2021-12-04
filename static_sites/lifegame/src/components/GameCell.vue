7738f0ed24148346090e6f3cc076ff66a1301fc3
kabik <k2.s1k.x20@gmail.com>
2021-12-05 00:13:41 +0900

    GameCell

---
+++ static_sites/lifegame/src/components/GameCell.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="game-cell"
+    @click="$emit('clik')"
+    :style="{
+      backgroundColor: owner === 'red' ? `red` : 'blue'
+    }"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    owner: { type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
